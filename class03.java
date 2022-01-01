7d28bb0d4f248137e5936c9daaa7b3ed20ea5eb1
qertyuiiopskdl <92702938+qertyuiiopskdl@users.noreply.github.com>
2022-01-01 20:37:00 +0800

    Add files via upload

---
+++ class03.java
@@ -0,0 +1,21 @@
+package java13;
+public class class03
+ {
+ public static void main(String args[])
+ {
+ try
+ {
+ int num=12,den=0;
+ int ans=num/den;
+ System.out.println("ans="+ans);
+ }
+ catch(ArithmeticException e)
+ {
+ System.out.println("°£¼Æ¬° 0");
+ }
+ finally
+ {
+ System.out.println("end of main() method!!");
+ }
+ }
+ }
